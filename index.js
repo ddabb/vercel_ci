74e0476716068d38da6ef80c63ba729aa6500f5b
60138546 <1011888891@qq.com>
2024-07-28 10:50:02 +0800

    测试初始化

--- index.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(request, response) {
-  try {
-
-    response.status(200).send('欢迎来到 60score.com');
-  } catch (error) {
-    console.error('捕获到全局错误:', error.message);
-    response.status(500).send(`发生内部服务器错误。${error.message}`);
-  }
-}
