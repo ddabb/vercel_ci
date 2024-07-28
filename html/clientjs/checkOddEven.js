e6aadc0b9dd0ff9ed5ef4827f34d5b9940a0c159
ddabb <1011888891@qq.com>
2024-07-28 06:31:18 +0000

    优化页面效果

--- html/clientjs/checkOddEven.js
+++ html/clientjs/checkOddEven.js
@@ -1,24 +1,27 @@
 // 定义一个函数来发送请求并处理响应
 async function checkOddEven() {
-    // 获取用户输入的数字
-    const numberInput = document.getElementById('numberInput').value;
-    try {
-      // 发送POST请求到服务器端点
-      const response = await fetch('/api/oddEven', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ body: parseInt(numberInput, 10) })
-      });
-  
-      // 解析响应
-      const data = await response.json();
-  
-      // 更新页面上的内容
-      document.getElementById('result').textContent = data.message;
-    } catch (error) {
-      console.error('Error:', error);
-      document.getElementById('result').innerHTML = '发生错误，请重试';
-    }
+  // 获取用户输入的数字
+  const numberInput = document.getElementById("numberInput").value;
+
+  try {
+    // 发送 POST 请求到服务器端点
+    const response = await fetch("/api/oddEven", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ number: parseInt(numberInput, 10) }), 
+    });
+
+    // 解析响应
+    const data = await response.json();
+
+    // 更新页面上的内容
+    const resultElement = document.getElementById("result"); 
+    resultElement.textContent = `你输入的 ${numberInput} 是一个${data.message}`; 
+    resultElement.style.color = data.message === "奇数"? "red" : "green"; 
+  } catch (error) {
+    console.error("Error:", error);
+    document.getElementById("result").innerHTML = "发生错误，请重试";
   }
+}
