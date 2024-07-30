3f17e815f9d5e047eaacd1bce7b724d1828ce25a
ddabb <1011888891@qq.com>
2024-07-30 09:32:23 +0000

    543543

--- api/getmdfiles.js
+++ api/getmdfiles.js
@@ -1,13 +1,23 @@
 import fs from 'fs';
 import path from 'path';
+
 export default async function handler(request, response) {
-    try {
-        const mdFiles = fs.readdirSync('../mdfiles', { withFileTypes: true })
-       .filter((file) => file.isFile() && path.extname(file.name) === '.md')
-       .map((file, index) => ({ name: file.name, order: index + 1 }));
+    const mdFilesDirectory = path.resolve(__dirname, '..', 'mdfiles');
+  try {
+    // 假设你的 Node.js 应用程序是从项目的根目录启动的
+
     
-        response.status(200).json(mdFiles);
-    } catch (error) {
-        response.status(500).json({ 具体错误: error});
+    // 检查目录是否存在
+    if (!fs.existsSync(mdFilesDirectory)) {
+      throw new Error(`Directory '${mdFilesDirectory}' does not exist.`);
     }
+    
+    const mdFiles = fs.readdirSync(mdFilesDirectory, { withFileTypes: true })
+      .filter((file) => file.isFile() && path.extname(file.name) === '.md')
+      .map((file, index) => ({ name: file.name, order: index + 1 }));
+
+    response.status(200).json(mdFiles);
+  } catch (error) {
+    response.status(500).json({ 具体错误: error.message+mdFilesDirectory });
+  }
 }
