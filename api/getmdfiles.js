e69582d1fac98b794798c20ad0013deeef18af59
ddabb <1011888891@qq.com>
2024-07-30 09:26:45 +0000

    545

--- api/getmdfiles.js
+++ api/getmdfiles.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 export default async function handler(request, response) {
     try {
-        const mdFiles = fs.readdirSync('./mdfiles', { withFileTypes: true })
+        const mdFiles = fs.readdirSync('../mdfiles', { withFileTypes: true })
        .filter((file) => file.isFile() && path.extname(file.name) === '.md')
        .map((file, index) => ({ name: file.name, order: index + 1 }));
     
