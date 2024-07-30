59e2116378d3b50d62f1eceb8c55f47f99d0da7a
ddabb <1011888891@qq.com>
2024-07-30 09:26:31 +0000

    443

--- api/getmdfiles.js
+++ api/getmdfiles.js
@@ -1,9 +1,13 @@
 import fs from 'fs';
 import path from 'path';
 export default async function handler(request, response) {
-    const mdFiles = fs.readdirSync('./mdfiles', { withFileTypes: true })
-     .filter((file) => file.isFile() && path.extname(file.name) === '.md')
-     .map((file, index) => ({ name: file.name, order: index + 1 }));
+    try {
+        const mdFiles = fs.readdirSync('./mdfiles', { withFileTypes: true })
+       .filter((file) => file.isFile() && path.extname(file.name) === '.md')
+       .map((file, index) => ({ name: file.name, order: index + 1 }));
     
-    response.status(200).json(mdFiles);
+        response.status(200).json(mdFiles);
+    } catch (error) {
+        response.status(500).json({ 具体错误: error});
+    }
 }
