4ef2f50173f8db41c5c3ff594e3cb327f313c541
ddabb <1011888891@qq.com>
2024-07-31 13:15:55 +0000

    Update mdfiles.json

--- api/articles.js
+++ api/articles.js
@@ -6,7 +6,7 @@ export default async function handler(request, response) {
   
     // 从请求中获取关键词和分页的信息
     const keyword = request.query.keyword || '';
-    const pageSize = parseInt(request.query.pageSize) || 5;
+    const pageSize = parseInt(request.query.pageSize) || 10;
     const page = parseInt(request.query.page) || 1;
     const startIndex = (page - 1) * pageSize;
   
--- js/article.js
+++ js/article.js
@@ -5,11 +5,10 @@ document.addEventListener('DOMContentLoaded', function() {
     debugger
     const urlParams = new URLSearchParams(window.location.search);
     const articleName = urlParams.get('name');
-
-    fetch(`/api/wenzhang/${articleName}`)
+    
+    fetch(`/api/wenzhang?name=${articleName}`) // 使用查询参数传递文章名称
         .then(response => response.json())
         .then(data => {
-            debugger
             console.log(data);
             document.getElementById('article-title').textContent = data.title;
             document.getElementById('article-content').innerHTML = data.content;
