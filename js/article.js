c157a00ff5f1e2e2ee83961f032e7c92642014d9
ddabb <1011888891@qq.com>
2024-07-31 13:05:57 +0000

    Update mdfiles.json

--- js/article.js
+++ js/article.js
@@ -1,12 +1,16 @@
 // public/js/article.js
 
 document.addEventListener('DOMContentLoaded', function() {
+
+    debugger
     const urlParams = new URLSearchParams(window.location.search);
     const articleName = urlParams.get('name');
 
     fetch(`/api/wenzhang/${articleName}`)
         .then(response => response.json())
         .then(data => {
+            debugger
+            console.log(data);
             document.getElementById('article-title').textContent = data.title;
             document.getElementById('article-content').innerHTML = data.content;
         })
