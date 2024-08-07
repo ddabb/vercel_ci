523ed034e1396c132608e884e7bb9ba3699fa16c
60138546 <1011888891@qq.com>
2024-08-07 11:17:23 +0800

    Update mdfiles.json

--- js/index.js
+++ js/index.js
@@ -1,27 +1,27 @@
 // public/js/index.js
 
 document.addEventListener('DOMContentLoaded', function () {
-  const searchInput = document.getElementById('search-input');
-  const searchButton = document.getElementById('search-button');
+  const searchInputMD = document.getElementById('search-input-md');
+  const searchButtonMD = document.getElementById('search-button-md');
+  const searchInputWeixin = document.getElementById('search-input-weixin');
+  const searchButtonWeixin = document.getElementById('search-button-weixin');
 
-  // 当有人按下“找找看”按钮，我们就去魔法书里找文章
-  searchButton.addEventListener('click', () => {
-    fetchArticles(searchInput.value);
+  // 网站文章的处理
+  searchButtonMD.addEventListener('click', () => {
+    fetchArticlesMD(searchInputMD.value);
   });
+  fetchArticlesMD(); // 初始化时加载所有文章
 
-  fetchArticles(); // 初始化时加载所有文章
-
-  async function fetchArticles(keyword = '') {
+  async function fetchArticlesMD(keyword = '') {
     const response = await fetch(`/api/articles?calltype=md&keyword=${encodeURIComponent(keyword)}`);
     const data = await response.json();
-    displayArticles(data.articles);
-    createPagination(data.currentPage, data.totalPages, data.pageSize);
+    displayArticlesMD(data.articles);
+    createPaginationMD(data.currentPage, data.totalPages, data.pageSize);
   }
 
 
-
-  function displayArticles(articles) {
-    const articleList = document.getElementById('article-list');
+  function displayArticlesMD(articles) {
+    const articleList = document.getElementById('article-list-md');
     articleList.innerHTML = ''; // 清空旧的文章列表
 
     articles.forEach(article => {
@@ -29,13 +29,12 @@ document.addEventListener('DOMContentLoaded', function () {
       link.href = `/html/article.html?name=${encodeURIComponent(article.name.replace('.md', ''))}`;
       link.textContent = article.title || article.name.replace('.md', '');
       articleList.appendChild(link);
-      // articleList.appendChild(document.createElement('br'));
     });
   }
 
   // 创建分页的函数
-  function createPagination(currentPage, totalPages, pageSize) {
-    const pagination = document.getElementById('pagination');
+  function createPaginationMD(currentPage, totalPages, pageSize) {
+    const pagination = document.getElementById('pagination-md');
     pagination.innerHTML = ''; // 清空旧的分页
 
     for (let i = 1; i <= totalPages; i++) {
@@ -45,7 +44,7 @@ document.addEventListener('DOMContentLoaded', function () {
         fetch(`/api/articles?calltype=md&page=${i}&pageSize=${pageSize}`)
           .then(response => response.json())
           .then(data => {
-            displayArticles(data.articles);
+            displayArticlesMD(data.articles);
           });
       };
 
@@ -57,6 +56,60 @@ document.addEventListener('DOMContentLoaded', function () {
       pagination.appendChild(document.createTextNode(' ')); // 添加空格间隔
     }
   }
+
+  // 公众号文章的处理
+  searchButtonWeixin.addEventListener('click', () => {
+    fetchArticlesWeixin(searchInputWeixin.value);
+  });
+  fetchArticlesWeixin(); // 初始化时加载所有文章
+
+  async function fetchArticlesWeixin(keyword = '') {
+    const response = await fetch(`/api/articles?calltype=weixin&keyword=${encodeURIComponent(keyword)}`);
+    const data = await response.json();
+    displayArticlesWeixin(data.articles);
+    createPaginationWeixin(data.currentPage, data.totalPages, data.pageSize);
+  }
+
+  function displayArticlesWeixin(articles) {
+    const articleList = document.getElementById('article-list-weixin');
+    articleList.innerHTML = ''; // 清空旧的文章列表
+
+    articles.forEach(article => {
+      const link = document.createElement('a');
+      link.href = article.url;
+      link.textContent = article.title;
+      link.target = "_blank";
+      articleList.appendChild(link);
+    });
+  }
+
+  function createPaginationWeixin(currentPage, totalPages, pageSize) {
+    const pagination = document.getElementById('pagination-weixin');
+    pagination.innerHTML = ''; // 清空旧的分页
+
+    for (let i = 1; i <= totalPages; i++) {
+      const button = document.createElement('button');
+      button.textContent = i;
+      button.onclick = () => {
+        fetch(`/api/articles?calltype=weixin&page=${i}&pageSize=${pageSize}`)
+          .then(response => response.json())
+          .then(data => {
+            displayArticlesWeixin(data.articles);
+          });
+      };
+
+      if (i === currentPage) {
+        button.classList.add('active');
+      }
+
+      pagination.appendChild(button);
+      pagination.appendChild(document.createTextNode(' ')); // 添加空格间隔
+    }
+  }
+
+
+
+
 });
 
 
