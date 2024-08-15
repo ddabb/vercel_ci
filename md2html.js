ce28265edd015f5c4805c4a84d2b9618a089e81d
ddabb <1011888891@qq.com>
2024-08-15 02:48:06 +0000

    Update mdfiles.json and sitemap.xml

--- md2html.js
+++ md2html.js
@@ -35,10 +35,6 @@ async function updateHtmlFile(htmlFilePath, htmlContent, outputFilePath) {
   console.log(`Updating HTML file: ${outputFilePath}`);
   try {
     const updatedHtml = await ejs.renderFile(htmlFilePath, { articleContent: htmlContent }, { escape: false });
-    if (outputFilePath === 'mdhtml/聊一下乒乓混双得主王楚钦和邓颖莎.html') {
-      console.log(`htmlContent ${htmlContent}`);
-      console.log(`updatedHtml ${updatedHtml}`);
-    }
     await fs.writeFile(outputFilePath, updatedHtml);
   } catch (error) {
     console.error(`Error updating HTML file: ${outputFilePath}`, error);
@@ -63,19 +59,8 @@ async function mdToHtml(mdFilesDirectory = 'mdfiles', genhtmlDirectory = 'mdhtml
     for (const fileName of markdownFiles) {
       const mdFilePath = path.join(mdFilesDirectory, fileName);
       let htmlContent;
-      // 只为特定文件或第一个文件输出日志
-      if (fileName === 'your-target-file.md' || markdownFiles.indexOf(fileName) === 0) {
-        console.log(`Processing file: ${fileName}`); // 添加日志输出
-        const markdownContent = await readMarkdownFile(mdFilePath);
-        console.log(`Converted ${fileName} to HTML`); // 添加日志输出
-        htmlContent = convertMarkdownToHtml(markdownContent);
-        console.log(`htmlContent ${htmlContent}`); // 添加日志输出
-      } else {
-        const markdownContent = await readMarkdownFile(mdFilePath);
-        htmlContent = convertMarkdownToHtml(markdownContent);
-
-      }
-
+      const markdownContent = await readMarkdownFile(mdFilePath);
+      htmlContent = convertMarkdownToHtml(markdownContent);
       // 生成单独的 .html 文件
       const outputFilePath = path.join(genhtmlDirectory, `${path.basename(fileName, '.md')}.html`);
       await updateHtmlFile(htmlTemplatePath, htmlContent, outputFilePath);
