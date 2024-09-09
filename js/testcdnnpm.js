97ff1f0635ab1ccd014a6b5f8df8487df554c9ee
60138546 <1011888891@qq.com>
2024-09-09 10:42:48 +0800

    Update mdfiles.json, sitemap.xml, and generate HTML files

--- js/testcdnnpm.js
+++ js/testcdnnpm.js
@@ -1,5 +1,5 @@
 // script.js
-import { Chat } from 'https://cdn.jsdelivr.net/npm/fishbb@1.0.5/+esm';
+import { chat } from 'https://cdn.jsdelivr.net/npm/fishbb@1.0.7/+esm';
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('query-form');
     form.addEventListener('submit', async (event) => {
@@ -8,8 +8,8 @@ document.addEventListener('DOMContentLoaded', () => {
         const responseDiv = document.getElementById('response');
 
         try {
-            const result = await Chat('hfr0hjaEDPYL', 'rodneyxiong', input.value);
-            responseDiv.innerHTML = `<p>${result.text}</p>`; // 假设结果有一个 text 属性
+            const result = await chat('hfr0hjaEDPYL', 'rodneyxiong', input.value);
+            responseDiv.innerHTML = `<p>${result}</p>`; // 假设结果有一个 text 属性
         } catch (error) {
             console.error('Failed to fetch chat completion:', error);
             responseDiv.innerHTML = '<p>There was an error processing your request.</p>';
