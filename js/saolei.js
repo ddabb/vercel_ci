4b7434f2a14d330c460f4710d5597f86609ccbb2
60138546 <1011888891@qq.com>
2024-10-04 14:12:16 +0800

    Update mdfiles.json, sitemap.xml, and generate HTML files

--- js/saolei.js
+++ js/saolei.js
@@ -1,261 +1,210 @@
-(function () {
-    var wrapper = document.getElementsByClassName('wrapper')[0];
-    var remain = document.getElementsByClassName('remain')[0];
-    var startBtn = document.getElementsByClassName('start-btn')[0];
-    var winBtn = document.getElementsByClassName('win')[0];
-    var loseBtn = document.getElementsByClassName('lose')[0];
-    var levelBtn = document.getElementsByClassName('level')[0];
-    var resetBtn = document.getElementsByClassName('reset')[0];
-    var timeBox = document.getElementsByClassName('time')[0];
-    var timer = null;
-    var box = null;
-    var active = document.getElementsByClassName('active')[0];
-    var mines,
-        Index,
-        key = true; // 控制游戏开始后是否还能点击开始按钮
-    var levelIndex = 'easy'; // 难度选择
+const wrapper = document.querySelector('.wrapper');
+const mines = document.querySelector('.mines .remain');
+const startBtn = document.querySelector('.start-btn');
+const resetBtn = document.querySelector('.reset');
+const levelList = document.querySelectorAll('.level li');
+const timeDisplay = document.querySelector('.time');
 
-    levelBtn.onclick = function (e) {
-        e = e || window.event;
-        target = e.target || e.srcElement;
-        if (target.tagName == "LI") {
-            if (key) {
-                active.classList.remove('active');
-                target.classList.add('active');
-                active = target;
-                if (target.classList.contains('easy')) {
-                    levelIndex = 'easy';
-                } else if (target.classList.contains('normal')) {
-                    levelIndex = 'normal';
-                } else if (target.classList.contains('hard')) {
-                    levelIndex = 'hard';
-                }
+let tabRowN = 10; // 行数
+let tabColN = 10; // 列数
+let mineN = 10; // 雷的数量
+let remainMineN = mineN; // 剩余雷数
+let openedTabN = 0; // 已打开的单元格数量
+let isGameOver = false;
+let timer = null;
+let seconds = 0;
+
+// 初始化游戏
+function initGame() {
+    console.log('初始化游戏');
+    clearInterval(timer);
+    seconds = 0;
+    timeDisplay.innerText = '00:00';
+    remainMineN = mineN;
+    openedTabN = 0;
+    isGameOver = false;
+    mines.innerText = remainMineN;
+    createMap(tabRowN, tabColN);
+    startTimer();
+}
+
+// 创建地图
+function createMap(tabRowN, tabColN) {
+    console.log('创建地图');
+    const tabGrid = document.createElement('div');
+    tabGrid.className = 'tab-grid';
+    wrapper.innerHTML = ''; // 清空之前的地图
+    wrapper.appendChild(tabGrid);
+
+    const totalTabs = tabRowN * tabColN;
+    const minePositions = new Set();
+    while (minePositions.size < mineN) {
+        const pos = Math.floor(Math.random() * totalTabs);
+        minePositions.add(pos);
+    }
+
+    const mines = {};
+    for (let pos of minePositions) {
+        const row = Math.floor(pos / tabColN);
+        const col = pos % tabColN;
+        mines[`${row}-${col}`] = true;
+    }
+
+    for (let i = 0; i < tabRowN; i++) {
+        for (let j = 0; j < tabColN; j++) {
+            const tab = document.createElement('div');
+            tab.className = 'tab';
+            tab.setAttribute('id', `${i}-${j}`);
+            if (mines[`${i}-${j}`]) {
+                tab.classList.add('bomb');
             }
+            tabGrid.appendChild(tab);
         }
     }
 
-    startBtn.onclick = startGame;
+    // 动态调整 grid-template-columns 和 grid-template-rows
+    tabGrid.style.gridTemplateColumns = `repeat(${tabColN}, 35px)`;
+    tabGrid.style.gridTemplateRows = `repeat(${tabRowN}, 35px)`;
 
-    resetBtn.addEventListener('mousedown', function () {
-        this.style.transform = "scale(0.8)";
-    }, false);
+    wrapper.style.width = `${tabColN * 35}px`;
+    wrapper.style.height = `${tabRowN * 35}px`;
 
-    resetBtn.addEventListener('mouseup', function () {
-        this.style.transform = "scale(1)";
-        restart();
-    }, false);
-
-    function startGame() {
-        if (key) {
-            init();
-            createTimer(timeBox);
-            key = false;
+    tabGrid.oncontextmenu = (e) => e.preventDefault();
+    tabGrid.addEventListener('mousedown', (e) => {
+        const target = e.target;
+        if (e.button === 0) {
+            leftClick(target);
+        } else if (e.button === 2) {
+            rightClick(target);
         }
-    }
+    });
+}
 
-    function init() {
-        switch (levelIndex) {
-            case 'easy':
-                this.tabRowN = 10;
-                this.tabColN = 10;
-                this.mineN = 10;
-                break;
-            case 'normal':
-                this.tabRowN = 16;
-                this.tabColN = 16;
-                this.mineN = 40;
-                break;
-            case 'hard':
-                this.tabRowN = 16;
-                this.tabColN = 30;
-                this.mineN = 99;
-                break;
-        }
-        this.flagN = 0;
-        Index = mineN;
-        mines = createMines();
-        createMap();
-    }
+// 开始计时
+function startTimer() {
+    console.log('开始计时');
+    timer = setInterval(() => {
+        seconds++;
+        const minutes = Math.floor(seconds / 60);
+        const secondsStr = (seconds % 60).toString().padStart(2, '0');
+        timeDisplay.innerText = `${minutes}:${secondsStr}`;
+    }, 1000);
+}
 
-    function createMap() {
-        box = document.createElement('div');
-        for (var i = 0; i < this.tabRowN; i++) {
-            for (var j = 0; j < this.tabColN; j++) {
-                var tab = document.createElement('div');
-                tab.className = 'tab';
-                tab.setAttribute('id', i + '-' + j);
-                for (var prop in mines) {
-                    if (prop == i + '-' + j) {
-                        tab.classList.add('bomb');
-                    }
-                }
-                box.appendChild(tab);
-            }
-        }
-        wrapper.style.width = this.tabColN * 35 + 'px';
-        wrapper.style.height = this.tabRowN * 35 + 'px';
-        wrapper.appendChild(box);
-        remain.innerText = this.mineN;
-        box.oncontextmenu = function () {
-            return false;
-        }
-        box.onmousedown = function (e) {
-            var e = e || window.event;
-            var target = e.target || e.srcElement;
-            if (e.which == 1) {
-                leftClick(target);
-            } else if (e.which == 3) {
-                rightClick(target);
-            }
-        }
-    }
+// 左键点击
+function leftClick(tab) {
+    console.log('左键点击');
+    if (isGameOver || tab.classList.contains('opened') || tab.classList.contains('flag')) return;
 
-    function createMines() {
-        var mines = {};
-        for (var i = 0; i < mineN;) {
-            var r = Math.floor(Math.random() * this.tabRowN);
-            var c = Math.floor(Math.random() * this.tabColN);
-            var num = r + '-' + c;
-            if (!mines[num]) {
-                mines[num] = 'bomb';
-                i++;
-            }
-        }
-        return mines;
-    }
-
-    function leftClick(target) {
-        if (target.classList.contains('flag')) return;
-        target.classList.add('lock');
-        if (target.classList.contains('bomb')) {
-            target.classList.add('show');
-            gameOver();
-        } else {
-            var rc = target.getAttribute('id').split('-');
-            var r, c, num = 0;
-            r = +rc[0];
-            c = +rc[1];
-            for (var i = r - 1; i <= r + 1; i++) {
-                for (var j = c - 1; j <= c + 1; j++) {
-                    if (i == r && j == c) continue;
-                    var dom = document.getElementById(i + '-' + j);
-                    if (dom && dom.classList.contains('bomb')) {
-                        num++;
-                    }
-                }
-            }
-            target.style.backgroundColor = "white";
-            if (num) {
-                target.innerText = num;
-            } else {
-                for (var i = r - 1; i <= r + 1; i++) {
-                    for (var j = c - 1; j <= c + 1; j++) {
-                        var aroundBox = document.getElementById(i + '-' + j);
-                        if (aroundBox) {
-                            if (!aroundBox.classList.contains('check')) {
-                                aroundBox.classList.add('check');
-                                leftClick(aroundBox);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    function rightClick(target) {
-        if (!target.classList.contains('lock')) {
-            if (this.flagN == this.Index && !target.classList.contains('flag')) {
-                return;
-            } else {
-                target.classList.toggle('flag');
-            }
-            if (target.classList.contains('flag')) {
-                if (target.classList.contains('bomb')) {
-                    this.mineN--;
-                    if (mineN == 0) {
-                        youWin();
-                    }
-                }
-                remain.innerText--;
-                this.flagN++;
-            } else if (!target.classList.contains('flag')) {
-                if (target.classList.contains('bomb')) {
-                    this.mineN++;
-                }
-                remain.innerText++;
-                this.flagN--;
-            }
-        }
-    }
-
-    function showAll() {
-        var len = box.children.length;
-        for (var i = 0; i < len; i++) {
-            if (box.children[i].classList.contains('bomb')) {
-                box.children[i].classList.add('show');
-            }
-        }
-    }
-
-    function youWin() {
-        winBtn.parentNode.style.display = 'block';
-        clearTimer();
-        winBtn.onclick = function () {
-            this.parentNode.style.display = 'none';
-            clearMap();
-            winBtn.onclick = false;
-        }
-    }
-
-    function gameOver() {
-        showAll();
-        clearTimer();
-        loseBtn.onclick = function () {
-            this.parentNode.style.display = 'none';
-            clearMap();
-            loseBtn.onclick = false;
-        }
-        setTimeout(function () {
-            loseBtn.parentNode.style.display = 'block';
-        }, 500)
-    }
-
-    function clearMap() {
-        if (wrapper.children[0]) {
-            wrapper.removeChild(box);
-        }
-        key = true;
-    }
-
-    function restart() {
-        clearMap();
-        clearTimer();
-        timeBox.innerHTML = "00:00";
-        key = true;
-    }
-
-    function createTimer(dom) {
+    if (tab.classList.contains('bomb')) {
+        tab.classList.add('show');
         clearInterval(timer);
-        dom.innerHTML = '00:00';
-        var s = 0;
-        var m = 0;
-        var seconds = 0;
-        var minutes = 0;
-        timer = setInterval(function () {
-            s++;
-            seconds = s > 9 ? (s < 60 ? s : '00') : '0' + s;
-            if (s > 59) {
-                m++;
-                s = 0;
+        isGameOver = true;
+        console.log('游戏结束，你输了');
+    } else {
+        openTab(tab);
+    }
+}
+
+// 右键点击
+function rightClick(tab) {
+    console.log('右键点击');
+    if (isGameOver || tab.classList.contains('opened')) return;
+
+    if (tab.classList.contains('flag')) {
+        tab.classList.remove('flag');
+        remainMineN++;
+    } else {
+        tab.classList.add('flag');
+        remainMineN--;
+    }
+    mines.innerText = remainMineN;
+}
+
+// 打开单元格
+function openTab(tab) {
+    console.log('打开单元格');
+    const [row, col] = tab.id.split('-').map(Number);
+    if (tab.classList.contains('opened') || tab.classList.contains('flag')) return;
+
+    let mineCount = 0;
+    for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+            if (i === 0 && j === 0) continue;
+            const newRow = row + i;
+            const newCol = col + j;
+            if (newRow >= 0 && newRow < tabRowN && newCol >= 0 && newCol < tabColN) {
+                const neighborTab = document.getElementById(`${newRow}-${newCol}`);
+                if (neighborTab && neighborTab.classList.contains('bomb')) {
+                    mineCount++;
+                }
             }
-            minutes = m < 10 ? '0' + m : m;
-            dom.innerHTML = minutes + ":" + seconds;
-        }, 1000)
+        }
     }
 
-    function clearTimer() {
+    tab.classList.add('opened');
+    openedTabN++;
+
+    if (mineCount > 0) {
+        tab.innerText = mineCount;
+    } else {
+        for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+                if (i === 0 && j === 0) continue;
+                const newRow = row + i;
+                const newCol = col + j;
+                if (newRow >= 0 && newRow < tabRowN && newCol >= 0 && newCol < tabColN) {
+                    const neighborTab = document.getElementById(`${newRow}-${newCol}`);
+                    if (neighborTab) {
+                        openTab(neighborTab);
+                    }
+                }
+            }
+        }
+    }
+
+    if (openedTabN === (tabRowN * tabColN - mineN)) {
         clearInterval(timer);
+        isGameOver = true;
+        console.log('游戏结束，你赢了');
     }
+}
 
-})();
+// 重置游戏
+function resetGame() {
+    console.log('重置游戏');
+    initGame();
+}
+
+// 选择难度
+function selectLevel(level) {
+    console.log(`选择难度: ${level}`);
+    switch (level) {
+        case 'easy':
+            tabRowN = 10;
+            tabColN = 10;
+            mineN = 10;
+            break;
+        case 'normal':
+            tabRowN = 16;
+            tabColN = 16;
+            mineN = 40;
+            break;
+        case 'hard':
+            tabRowN = 16;
+            tabColN = 30;
+            mineN = 99;
+            break;
+    }
+    levelList.forEach(li => li.classList.remove('active'));
+    document.querySelector(`.${level}`).classList.add('active');
+    initGame();
+}
+
+// 事件监听
+startBtn.addEventListener('click', () => initGame());
+resetBtn.addEventListener('click', () => resetGame());
+levelList.forEach(li => li.addEventListener('click', () => selectLevel(li.classList[0])));
+
+// 初始化默认难度
+selectLevel('easy');
