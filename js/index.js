aa1cf2d2943ab7e9ea20dee5fe37dc774a19a9e6
60138546 <1011888891@qq.com>
2024-10-30 17:03:00 +0800

    Update mdfiles.json, sitemap.xml, and generate HTML files

--- js/index.js
+++ js/index.js
@@ -1,114 +1,20 @@
 // public/js/index.js
 
 document.addEventListener('DOMContentLoaded', function () {
-  const searchInputMD = document.getElementById('search-input-md');
-  const searchButtonMD = document.getElementById('search-button-md');
-  const searchInputWeixin = document.getElementById('search-input-weixin');
-  const searchButtonWeixin = document.getElementById('search-button-weixin');
-
-  // 网站文章的处理
-  searchButtonMD.addEventListener('click', () => {
-    fetchArticlesMD(searchInputMD.value);
-  });
-  fetchArticlesMD(); // 初始化时加载所有文章
-
-  async function fetchArticlesMD(keyword = '') {
-    const response = await fetch(`/api/articles?calltype=md&keyword=${encodeURIComponent(keyword)}`);
-    const data = await response.json();
-    displayArticlesMD(data.articles);
-    createPaginationMD(data.currentPage, data.totalPages, data.pageSize);
-  }
 
 
-  function displayArticlesMD(articles) {
-    const articleList = document.getElementById('article-list-md');
-    articleList.innerHTML = ''; // 清空旧的文章列表
 
-    articles.forEach(article => {
-      // 使用encodeURIComponent对文章名称进行编码
-      const encodedArticleName = encodeURIComponent(article.name.replace('.md', '.html'));
-      const link = document.createElement('a');
-      // 构建直接指向mdhtml文件夹中HTML文件的链接
-      link.href = `/mdhtml/${encodedArticleName}`;
-      link.textContent = article.title || article.name.replace('.md', '');
-      articleList.appendChild(link);
-    });
-  }
 
-  // 创建分页的函数
-  function createPaginationMD(currentPage, totalPages, pageSize) {
-    const pagination = document.getElementById('pagination-md');
-    pagination.innerHTML = ''; // 清空旧的分页
 
-    for (let i = 1; i <= totalPages; i++) {
-      const button = document.createElement('button');
-      button.textContent = i;
-      button.onclick = () => {
-        fetch(`/api/articles?calltype=md&page=${i}&pageSize=${pageSize}`)
-          .then(response => response.json())
-          .then(data => {
-            displayArticlesMD(data.articles);
-          });
-      };
 
-      if (i === currentPage) {
-        button.classList.add('active');
-      }
 
-      pagination.appendChild(button);
-      pagination.appendChild(document.createTextNode(' ')); // 添加空格间隔
-    }
-  }
 
-  // 公众号文章的处理
-  searchButtonWeixin.addEventListener('click', () => {
-    fetchArticlesWeixin(searchInputWeixin.value);
-  });
-  fetchArticlesWeixin(); // 初始化时加载所有文章
 
-  async function fetchArticlesWeixin(keyword = '') {
-    const response = await fetch(`/api/articles?calltype=weixin&keyword=${encodeURIComponent(keyword)}`);
-    const data = await response.json();
-    displayArticlesWeixin(data.articles);
-    createPaginationWeixin(data.currentPage, data.totalPages, data.pageSize);
-  }
 
-  function displayArticlesWeixin(articles) {
-    const articleList = document.getElementById('article-list-weixin');
-    articleList.innerHTML = ''; // 清空旧的文章列表
 
-    articles.forEach(article => {
-      const link = document.createElement('a');
-      link.href = article.url;
-      link.textContent = article.title;
-      link.target = "_blank";
-      articleList.appendChild(link);
-    });
-  }
 
-  function createPaginationWeixin(currentPage, totalPages, pageSize) {
-    const pagination = document.getElementById('pagination-weixin');
-    pagination.innerHTML = ''; // 清空旧的分页
 
-    for (let i = 1; i <= totalPages; i++) {
-      const button = document.createElement('button');
-      button.textContent = i;
-      button.onclick = () => {
-        fetch(`/api/articles?calltype=weixin&page=${i}&pageSize=${pageSize}`)
-          .then(response => response.json())
-          .then(data => {
-            displayArticlesWeixin(data.articles);
-          });
-      };
 
-      if (i === currentPage) {
-        button.classList.add('active');
-      }
-
-      pagination.appendChild(button);
-      pagination.appendChild(document.createTextNode(' ')); // 添加空格间隔
-    }
-  }
 
 
 
@@ -155,7 +61,7 @@ fetchAndRenderTimeline();
 
 // 控制时间线的折叠
 const timelineToggle = new bootstrap.Collapse('#timeline', {
-  toggle: true
+  toggle: false
 });
 
 document.getElementById('toggleButton').addEventListener('click', function () {
