6b7eb1769ee375d5c557eb2cce008e9949a3efef
ddabb <1011888891@qq.com>
2025-03-03 01:49:54 +0000

    修复样式

--- js/Dynasty.js
+++ js/Dynasty.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.poet-list');
-    elems.forEach(function (elem) {
-        new Masonry(elem, {
-            itemSelector: '.poet-item',
-            gutter: 10,
-            fitWidth: true
-        });
-    });
+    // var elems = document.querySelectorAll('.poet-list');
+    // elems.forEach(function (elem) {
+    //     new Masonry(elem, {
+    //         itemSelector: '.poet-item',
+    //         gutter: 10,
+    //         fitWidth: true
+    //     });
+    // });
 });
