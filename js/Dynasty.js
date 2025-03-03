137ac5f7de3e207cf2a1880f73865b64da2435f5
ddabb <1011888891@qq.com>
2025-03-03 01:21:42 +0000

    优化整个页面的布局效果

---
+++ js/Dynasty.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.poet-list');
+    elems.forEach(function (elem) {
+        new Masonry(elem, {
+            itemSelector: '.poet-item',
+            gutter: 10,
+            fitWidth: true
+        });
+    });
+});
