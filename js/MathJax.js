e0b70715e758604db2b77bda1c2bd6cd5a46c9fe
ddabb <1011888891@qq.com>
2025-03-11 02:02:26 +0000

    提交代码

--- js/MathJax.js
+++ js/MathJax.js
@@ -1,14 +1,20 @@
 // MathJax.js
 MathJax = {
     tex: {
-        inlineMath: [['$', '$'], ['\$', '\$']],  // 配置行内公式分隔符
-        displayMath: [['$$', '$$'], ['\$', '\$']] // 配置块级公式分隔符
+      inlineMath: [['$', '$'], ['\$', '\$']],  // 配置行内公式分隔符
+      displayMath: [['$$', '$$'], ['\$', '\$']] // 配置块级公式分隔符
+    },
+    startup: {
+      ready: () => {
+        console.log('MathJax is loaded, ready for typesetting mathematics');
+        MathJax.startup.defaultReady();
+      }
     }
-};
-
-(function () {
+  };
+  
+  (function() {
     var script = document.createElement('script');
-    script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
+    script.src = '/js/mathjax/es5/tex-mml-chtml.js'; // 注意这里的路径要根据您的实际项目结构调整
     script.async = true;
     document.head.appendChild(script);
-})();
+  })();
